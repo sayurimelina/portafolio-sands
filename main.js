8a774eeb1f67c9f23ebe786c8ed8febe2eb6e9be
Sayuri Melina <cernasayuri@gmail.com>
2023-11-04 09:30:36 -0500

    added section prices web size

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', function () {
     var navbar = document.getElementById('navbar');
     var scroll = window.scrollY;
     if (scroll > 50) {
-        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
         navbar.style.backdropFilter = 'blur(90px)';
         navbar.style.boxShadow = '#fff'
     } else {
@@ -11,3 +11,11 @@ window.addEventListener('scroll', function () {
         navbar.style.backdropFilter = 'blur(0)';
     }
 }); 
+
+/*
+Desactiva el click derecho
+
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+*/
